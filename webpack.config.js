ef84efd67ce89c4422bc5eae46b59c78e45c3134
Joe B <joebanegas@gmail.com>
2019-01-03 09:37:25 -0600

    add webpack codesplitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
   const path = require('path');
 
   module.exports = {
-    entry: './src/js/index.js',
+    entry: {
+    	index: './src/js/index.js',
+    	another: './src/js/another-module.js'
+    }
     output: {
 		filename: 'main.js',
     	path: path.resolve(__dirname, 'dist/js')
-    }
+    },
+    optimization: {
+     	splitChunks: {
+       		chunks: 'all'
+    	 }
+   	}
   };
