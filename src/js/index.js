0d62deece6834819bb0a5a4838aee67a7058a639
Joe B <joebanegas@gmail.com>
2019-01-29 22:20:10 -0600

    GA integration

--- src/js/index.js
+++ src/js/index.js
@@ -4,35 +4,6 @@ const user = "jbanegas";
 const tag = "portfolio"
 const url = "https://cpv2api.com/pens/showcase/" + user;
 
-console.log(url)
-
-
- /*.full.noodles
-        h3 Noodlings
-        p Here's over 25 projects I've completed on the side, whether its keeping up with modern trends or answering questions on StackOverflow I enjoy taking time to code.
-       
-        ul
-            li
-                .image
-                h4 Project Title
-                p Lorem ipsum dolor sit amet, mel nonumy utamur ex, ei semper tritani aliquando has. Ei sint soleat graeci cum, ne dici
-                .num 01
-            li
-                .image
-                h4 Project Title
-                p Lorem ipsum dolor sit amet, mel nonumy utamur ex, ei semper tritani aliquando has. Ei sint soleat graeci cum, ne dici
-                .num 02
-            li
-                .image
-                h4 Project Title
-                p Lorem ipsum dolor sit amet, mel nonumy utamur ex, ei semper tritani aliquando has. Ei sint soleat graeci cum, ne dici
-                .num 03
-            li
-                .image
-                h4 Project Title
-                p Lorem ipsum dolor sit amet, mel nonumy utamur ex, ei semper tritani aliquando has. Ei sint soleat graeci cum, ne dici
-                .num 04*/
-
   
 function getPens(pens){
     const $penList = $(".noodles > ul");
@@ -47,8 +18,6 @@ function getPens(pens){
                 const details = pen.details
                 const link = pen.link
 
-                console.log(imageSrc);
-                console.log(pen)
                 $penList.append(
                     $("<li>")
                         .append($("<a href='" + link + "'>")
